66e00c149f54e44618709ebbfcabe92d2e5122d0
Marcel Blijleven <marcelblijleven@gmail.com>
2019-08-08 21:47:26 +0200

    Added sample service

---
+++ server/services.js
@@ -0,0 +1,16 @@
+// Example services
+
+const services = [
+  {
+    name: 'JSONPlaceHolder',
+    url: 'https://jsonplaceholder.typicode.com/todos/1',
+    healthyValue: {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    }
+  }
+]
+
+module.exports = services
