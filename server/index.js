ac6f0490fefd3f30c33572d11fb0df139ab79ee5
Marcel Blijleven <marcelblijleven@gmail.com>
2019-08-08 22:06:23 +0200

    Setting up socketio and pollServices

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const Express = require('express')
+const socket = require('socket.io')
 const bodyParser = require('body-parser')
 
+const Poller = require('./src/poller')
+const getHealthCheck = require('./src/get-healthcheck')
 const services = require('./services')
 
 const port = process.env.port || 5000
+const pollTime = 60 * 1000 // 60 seconds
 
 // Setup app
 const app = new Express()
@@ -26,3 +30,28 @@ app.get('/services', function(req, res) {
 let server = app.listen(port, function() {
   console.info('Running app on port', port)
 })
+
+// Setup socket server
+let io = socket(server)
+
+io.on('connection', function(socket) {
+  console.info('Socket connected with ID', socket)
+  socket.emit('services', services) // Emit services to client
+  pollServices(services) // Initial poll
+})
+
+function pollServices(services) {
+  for (const service of services) {
+    getHealthCheck(service)
+      .then((result) => {
+        io.sockets.emit('update service', result)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+}
+
+// Start polling
+const poller = new Poller(() => pollServices(services), pollTime)
+poller.start()
