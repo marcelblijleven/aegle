db1d40adb1ab8d6815576d56196bbca463ce7826
Marcel Blijleven <marcelblijleven@gmail.com>
2019-08-16 16:32:22 +0200

    Added routes

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+const routes = require('express').Router()
+const services = require('./services')
+const healthcheck = require('./healthcheck')
+
+routes.get('/', function(req, res) {
+  res.type('json')
+  res.status(200)
+  res.send({aegleJS_server: 'connected'})
+})
+
+routes.use('/services', services)
+routes.use('/healthcheck', healthcheck)
+
+module.exports = routes
