8e0b6ed547c06d1e24777ba1d5a602d13127359f
Marcel Blijleven <marcelblijleven@gmail.com>
2019-08-22 17:57:30 +0200

    refactor: split up updateWebhook functionality

--- server/src/update-webhook.js
+++ server/src/update-webhook.js
@@ -1,21 +1,41 @@
 const fetch = require('node-fetch')
 
-module.exports = async function(message) {
+async function postMessage(body) {
+  const options = {
+    method: 'post',
+    body: body,
+    headers: { 'Content-Type': 'application/json' }
+  }
+  const response = await fetch(process.env.WEBHOOK, options)
+  return response
+}
+
+function getBody(message) {
+  if (process.env.WEBHOOK.includes('slack')) {
+    return { "text": "message" }
+  }
+
+  throw new Error('Webhook not implemented. Currently only Slack is implemented')
+}
+
+async function updateWebhook(message) {
   if (!process.env.WEBHOOK) {
     console.error('Server: tried calling webhook update without configured webhook url')
     return
   }
 
   try {
-    const body = JSON.stringify({text: message})
     console.info('Server: sending message to webhook')
-    await fetch(process.env.WEBHOOK, {
-      method: 'POST',
-      body: body,
-      headers: { 'Content-Type': 'application/json' }
-    })
+    const body = getBody(message)
+    const response = await postMessage(body)
+
+    if (response.status !== 200) {
+      throw new Error(`Webhook return http status ${response.status}.`)
+    }
   }
   catch (error) {
-    console.error(error.message)
+    console.error('Server:', error.message)
   }
 }
+
+module.exports = updateWebhook
