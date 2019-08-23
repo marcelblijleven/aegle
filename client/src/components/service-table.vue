7a6754d16397efcbd75447b21de95bb00f8ffd00
Marcel Blijleven <marcelblijleven@gmail.com>
2019-08-23 08:47:09 +0200

    feat: created service table component

---
+++ client/src/components/service-table.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="service-table">
+    <table>
+      <thead>
+          <tr>
+            <th>Service</th>
+            <th>Status</th>
+            <th>Modified at</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+  </div>
+</template>
