ee83247737fd85ec6f4469e4f2fc1ab1f8361d99
Marcel Blijleven <marcelblijleven@gmail.com>
2019-09-01 18:15:09 +0200

    feat: moved toolbar to own component, implemented dark mode

---
+++ client/src/components/Toolbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar app>
+        <v-toolbar-title>{{ title }}</v-toolbar-title>
+        <div class="flex-grow-1"></div>
+        <v-toolbar-items>
+          <dark-mode-toggle />
+        </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<script>
+import DarkModeToggle from './DarkModeToggle.vue'
+
+export default {
+  props: {
+    title: String,
+  },
+  components: {
+    DarkModeToggle,
+  }
+}
+</script>
