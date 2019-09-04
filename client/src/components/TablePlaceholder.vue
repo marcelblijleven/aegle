241cf6fb12a392964345fcda47963a154bdaa968
Marcel Blijleven <marcelblijleven@gmail.com>
2019-09-04 12:19:35 +0200

    feat: made server error placeholder a v-card

--- client/src/components/TablePlaceholder.vue
+++ client/src/components/TablePlaceholder.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="table-placeholder">
-    <div v-if="!connected">
-      <p>No connection to the Aegle server</p>
-      <p>Server: {{ server }}</p>
-    </div>
-    <div v-else-if="!hasServices">No services configured on the server</div>
+    <v-card v-if="!connected || !hasServices">
+      <v-card-title>Oops!</v-card-title>
+      <v-card-text class="text-left" v-if="!connected">
+        No connection to the socket server ({{ server }}).
+      </v-card-text>
+      <v-card-text class="text-left" v-else-if="!hasServices">
+        No services configured on the server.
+      </v-card-text>
+    </v-card>
     <table-component v-else :services="services" :columns="columns" />
   </div>
 </template>
