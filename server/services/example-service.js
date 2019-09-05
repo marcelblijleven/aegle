c252558470f54c9b664b311473696f827c8aed0f
Marcel Blijleven <marcelblijleven@gmail.com>
2019-09-05 17:29:14 +0200

    feat: added dynamic service creation

--- server/services/example-service.js
+++ server/services/example-service.js
@@ -2,20 +2,25 @@ const services = [
   {
     name: 'HttpStatus200 - after sleep 10 sec',
     url: 'https://httpstat.us/200?sleep=10000',
-    healthyValue: '200 OK',
-    type: 'text'
+    healthyValue: '200 OK'
   },
   {
     name: 'HttpStatus524',
     url: 'https://httpstat.us/524?sleep=4000',
-    healthyValue: '200 OK',
-    type: 'text'
+    healthyValue: '200 OK'
   },
   {
     name: 'HttpStatus200',
     url: 'https://httpstat.us/200',
-    healthyValue: '200 OK',
-    type: 'text'
+    healthyValue: '200 OK'
+  },
+  {
+    name: 'Dynamic service url and path',
+    url: 'https://{host}/{path}',
+    params: {
+      host: ['example.com', 'example.org'],
+      path: ['hello', 'world', 'foo', 'bar']
+    }
   }
 ]
 
