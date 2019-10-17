2b25e251fbccc49e6f44baf4edc4c22104aa4eb7
Marcel Blijleven <marcelblijleven@gmail.com>
2019-10-17 16:34:08 +0200

    fix: reverted changes in example service

--- server/services/example-service.js
+++ server/services/example-service.js
@@ -1,7 +1,26 @@
 const services = [
   {
     name: 'HttpStatus200 - after sleep 10 sec',
-    url: 'https://flux-capacitor.com:8080',
+    url: 'https://httpstat.us/200?sleep=10000',
+    healthyValue: '200 OK'
+  },
+  {
+    name: 'HttpStatus524',
+    url: 'https://httpstat.us/524?sleep=4000',
+    healthyValue: '200 OK'
+  },
+  {
+    name: 'HttpStatus200',
+    url: 'https://httpstat.us/200',
+    healthyValue: '200 OK'
+  },
+  {
+    name: 'dynamic-service',
+    url: 'https://{host}/{path}',
+    params: {
+      host: ['example.com', 'example.org'],
+      path: ['hello', 'world', 'foo', 'bar']
+    }
   }
 ]
 
